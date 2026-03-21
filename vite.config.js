19fa135f4545364cd32c76fb093deb3c86f8c738
yasmine-sassi <yasmine.sassi@insat.ucar.tn>
2026-03-21 19:35:42 +0100

    github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserOrOrgPages = repositoryName.endsWith(".github.io");
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS
+    ? isUserOrOrgPages
+      ? "/"
+      : `/${repositoryName}/`
+    : "/",
   plugins: [react()],
   resolve: {
     alias: {
